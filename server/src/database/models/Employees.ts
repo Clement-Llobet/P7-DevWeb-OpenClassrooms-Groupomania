2a14c98827d128a7098111157d1b4a318faae4e1
Clément Llobet <clement.llobet@tutanota.com>
2022-05-25 19:30:03 +0200

    create Employees model

---
+++ server/src/database/models/Employees.ts
@@ -0,0 +1,51 @@
+import { DataTypes, Model, Optional } from "sequelize";
+import sequelizeConnection from "../config";
+
+interface EmployeesAttributes {
+    id: number;
+    name: string;
+    surname: string;
+    email: string;
+    password: string;
+    moderation: number;
+}
+
+export interface EmployeesInput extends Optional<EmployeesAttributes, 'id'>{}
+export interface EmployeesOutput extends Required<EmployeesAttributes>{}
+
+class Employees extends Model<EmployeesAttributes, EmployeesInput> implements EmployeesAttributes {
+    public id!: number;
+    public name!: string;
+    public surname!: string;
+    public email!: string;
+    public password!: string;
+    public moderation!: number;
+}
+
+Employees.init({
+    id: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    name: {
+        type: DataTypes.STRING
+    },
+    surname: {
+        type: DataTypes.STRING
+    },
+    email: {
+        type: DataTypes.STRING
+    },
+    password: {
+        type: DataTypes.STRING
+    },
+    moderation: {
+        type: DataTypes.TINYINT
+    }
+}, {
+    sequelize: sequelizeConnection,
+    paranoid: true
+})
+
+export default Employees
