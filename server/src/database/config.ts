1593e0d3c55ce07a5945ebc49f5e518cae6e88f4
Clément Llobet <clement.llobet@tutanota.com>
2022-06-08 15:31:55 +0200

    fix db issue

--- server/src/database/config.ts
+++ server/src/database/config.ts
@@ -1,22 +1,29 @@
 require("dotenv").config();
-import { Dialect, Sequelize } from 'sequelize'
-import config from "../config/config"
+import { Dialect, Sequelize } from 'sequelize';
+// import "../config/config.json"
 
-const dbName = config.development.name as string
+// const dbName = config.development.name as string
 
-const dbUser = config.development.username as string;
-const dbPassword = config.development.password as string;
-const dbHost = config.development.host;
-const dbDriver = config.development.dialect;
+// const dbUser = config.development.username as string;
+// const dbPassword = config.development.password
+// const dbHost = config.development.host;
+// const dbDriver = config.development.dialect;
+console.log(process.env);
 
-// const dbUser = process.env.DB_USER as string
-// const dbPassword = process.env.DB_PASSWORD
-// const dbHost = process.env.DB_HOST
-// // const dbDriver = process.env.DB_DRIVER as Dialect
+const dbName = process.env.DB_NAME as string
+const dbUser = process.env.DB_USER as string
+const dbPassword = process.env.DB_PASSWORD
+const dbHost = process.env.DB_HOST
+const dbDriver = "mysql"
 
+// const sequelizeConnection = new Sequelize("mysql://localhost:3306/database", {
 const sequelizeConnection = new Sequelize(dbName, dbUser, dbPassword, {
   host: dbHost,
   dialect: dbDriver as Dialect
 })
+  // .authenticate()
+  //   .then(() => console.log("Connexion à la BDD Groupomania réussie."))
+  //   .catch(() => console.log("Echec de la connexxion à la BDD Groupomania"));
 
 export default sequelizeConnection
+
