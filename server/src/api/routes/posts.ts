810358d96db0689245bf3e4960707fb539306865
Clément Llobet <clement.llobet@tutanota.com>
2022-07-13 16:17:53 +0200

    create posts routes

---
+++ server/src/api/routes/posts.ts
@@ -0,0 +1,10 @@
+import express from "express";
+const router = express.Router();
+
+const postsController = require('../controllers/Posts/posts');
+
+router.put('/:id', postsController.updatePost);
+router.post('/', postsController.createPost)
+router.delete('/:id', postsController.deletePostById)
+router.get('/:id', postsController.getPostById);
+router.get('/', postsController.getAllPosts);
