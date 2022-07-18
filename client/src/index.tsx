f86a84754a00fe377f9443a2890126420e8d4532
Clément Llobet <clement.llobet@tutanota.com>
2022-07-18 19:53:37 +0200

    delete Router component and put routes in index.tsx

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,14 +1,27 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
-import Router from './components/Router';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Error from './components/Error';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Router />
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/error" element={<Error />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   </React.StrictMode>
 );
-reportWebVitals();
