8bcfe701f52b17cc7f738216a8336d96caca4e7e
Clément Llobet <clement.llobet@tutanota.com>
2022-07-24 16:40:10 +0200

    Create functions to add to controller methods

--- server/src/api/services/EmployeesServices.ts
+++ server/src/api/services/EmployeesServices.ts
@@ -1,19 +1,23 @@
 import * as employeesDal from "../../database/dal/employees.dal";
-// import { GetAllEmployeesFilters } from "../../database/dal/types";
+import { GetAllEmployeesFilters } from "../../database/dal/types";
 import { EmployeesInput, EmployeesOutput } from "../../database/models/Employees.model";
 
-export const create = (payload: EmployeesInput): Promise<EmployeesOutput> => {
+export const createEmployee = (payload: EmployeesInput): Promise<EmployeesOutput> => {
     return employeesDal.createEmployee(payload);
 }
 
-// export const update = (id: number, payload: Partial<EmployeesInput>): Promise<EmployeesOutput> => {
-//     return employeesDal.updateEmployee(id, payload);
-// }
+export const updateEmployee = (id: number, payload: Partial<EmployeesInput>): Promise<EmployeesOutput> => {
+    return employeesDal.updateEmployee(id, payload);
+}
 
-// export const getEmployeesById = (id: number): Promise<EmployeesOutput> => {
-//     return employeesDal.getEmployeeById(id);
-// }
+export const deleteEmployee = (id: number) => {
+    return employeesDal.deleteEmployeeById(id);
+}
 
-// export const getAllEmployees = (filters: GetAllEmployeesFilters): Promise<EmployeesOutput[]> => {
-//     return employeesDal.getAll(filters);
-// }
+export const getEmployeesById = (id: number): Promise<EmployeesOutput> => {
+    return employeesDal.getEmployeeById(id);
+}
+
+export const getAllEmployees = (filters: GetAllEmployeesFilters): Promise<EmployeesOutput[]> => {
+    return employeesDal.getAll(filters);
+}
