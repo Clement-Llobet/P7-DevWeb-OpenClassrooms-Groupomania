709d8cca39527006479841c7515a139904ccf523
Clément Llobet <clement.llobet@tutanota.com>
2022-08-21 23:20:31 +0200

    Add useParamas to get Id in Post deleteModal component

--- client/src/components/Posts.components/post.DeleteModal.tsx
+++ client/src/components/Posts.components/post.DeleteModal.tsx
@@ -1,33 +1,20 @@
-import { useState } from 'react';
-import { PostsData } from '../../interfaces';
+import { useParams } from 'react-router-dom';
 import { ApiService } from '../../service/api.service';
+import { PostIdParams } from '../../types/postId.type';
 
 const api = new ApiService(process.env.REACT_APP_REMOTE_SERVICE_BASE_URL);
 
 const DeleteModal: React.FC = () => {
-  const [wantToDelete, setWantToDelete] = useState(false);
+  const { id } = useParams<PostIdParams>();
 
   const deletePost = async () => {
-    setWantToDelete(true);
-
-    const postToDelete: PostsData = {
-      // A SUPPRIMER
-      id: 1,
-      text: '',
-      urlImage: '',
-      EmployeeId: 2,
-    };
-
-    if (wantToDelete) {
-      // await api.apiDeletePost(postToDelete); // IL FAUT PASSER EN PARAMETRE L'ID DU POST !!!
-    }
+    await api.apiDeletePost(id);
   };
 
   return (
     <div>
       <h3>Voulez-vous vraiment supprimer ce post ?</h3>
-      <button onClick={() => deletePost}>Oui</button>
-      <button onClick={() => setWantToDelete(false)}>Non</button>
+      <button onClick={() => deletePost()}>Confirmer</button>
     </div>
   );
 };
