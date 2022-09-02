0fe8779e43e841bdee9da0b9fd12eed5b349398c
Clément Llobet <clement.llobet@tutanota.com>
2022-09-02 20:34:20 +0200

    revert posts order

--- client/src/components/Posts.components/PostsList.tsx
+++ client/src/components/Posts.components/PostsList.tsx
@@ -8,27 +8,29 @@ interface PostsListProps {
 const PostsList: React.FC<PostsListProps> = ({ allPosts }) => {
   const navigate = useNavigate();
 
-  allPosts?.forEach((post) => {
-    console.log(post.author);
-  });
-
   return (
     <ul className="Posts">
-      {allPosts?.map((post) => (
-        <li key={post!.id} onClick={() => navigate(`/${post!.id}`)}>
-          <h3>
-            {post.profilePicture && post.profilePicture}
-            {post.author?.surname} {post.author?.name}
-          </h3>
-          <>Date : {post.author?.createdAt}</>
-          <p>{post.text}</p>
-          {/* <img src={post.urlImage} alt="" /> */}
-          <>
-            <button>Like</button>
-            <p>{post.likers!.length}</p>
-          </>
-        </li>
-      ))}
+      {allPosts
+        ?.reverse()
+        .map((post) => (
+          <li key={post!.id} onClick={() => navigate(`/${post!.id}`)}>
+            <h3>
+              {post.profilePicture && post.profilePicture}
+              {post.author?.surname} {post.author?.name}
+            </h3>
+            <>Date : {post.author?.createdAt}</>
+            <p>{post.text}</p>
+            {/* <img src={post.urlImage} alt="" /> */}
+            <>
+              <button>
+                Like
+                {/* {isAlreadyLiked ? "UnLike" : "Like"} */}
+              </button>
+              <p>{post.likers!.length}</p>
+            </>
+          </li>
+        ))
+        .reverse()}
     </ul>
   );
 };
