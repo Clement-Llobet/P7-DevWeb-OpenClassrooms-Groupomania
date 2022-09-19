a488d91541c6697cfaf4d884ce864325e15d414e
Clément Llobet <clement.llobet@tutanota.com>
2022-09-19 20:17:36 +0200

    add Lato font

--- client/src/indexStyle.ts
+++ client/src/indexStyle.ts
@@ -1,8 +1,12 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
+
+    @import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;1,400;1,700&display=swap');
+
     * {
         box-sizing: border-box;
+        font-family: 'Lato', sans-serif!important;
     }
 
     html, body, div, span, applet, object, iframe,
